b5b389d4797d49de37f57f09edbf7c8e7259d520
Jonathan Suarez <jaysuarezap@gmail.com>
2026-04-27 21:38:04 -0500

    Correccion connection.php

--- db/conexion.php
+++ db/conexion.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "unideportes-system"; // en phpMyAdmin
+$db   = "unideportes"; // en phpMyAdmin
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
